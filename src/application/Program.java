38539248f50a7028447a7294db4e7f47943e2932
Joao Menezes <joaovitormenezesm12@gmail.com>
2022-10-13 14:22:47 -0300

    Implementação da interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,26 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import model.entities.Product;
+import model.util.UpperCaseName;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.0));
+		list.add(new Product("Mouse", 50.0));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+
+		names.forEach(System.out::println);
 
 	}
-
 }
